fb13d40bbf1d79154d534c158e96b5d4f9e9c080
Carlos Tur <Carlos.tur@bosonit.com>
2025-12-18 11:21:36 +0100

    Spark.scala y Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,25 @@
-class Main {
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{SparkSession, Dataset}
 
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions.avg
+import java.io.File
+
+object MainApp {
+  def main(args: Array[String]): Unit = {
+
+    // Obtener SparkSession usando la función por defecto de Spark.scala
+    val spark: SparkSession = Spark.getSparkSession()
+    import spark.implicits._
+    // Ejemplo: mostrar versión de Spark
+    println(s"Versión de Spark: ${spark.version}")
+
+    // Aquí puedes poner el resto de tu código usando `spark`
+    // Por ejemplo, leer CSV, procesar DataFrame, etc.
+
+    // Cerrar SparkSession al final
+    spark.stop()
+  }
 }
--- src/main/scala/Spark.scala
+++ src/main/scala/Spark.scala
@@ -1,3 +1,21 @@
-class Spark {
-
+import org.apache.spark.sql.SparkSession
+object Spark {
+  def getSparkSession(name: String = "spark-scala-app", hive: Boolean = false):SparkSession ={
+    if(hive){
+      SparkSession
+        .builder()
+        .master("local[*]")
+        .appName(name)
+        .config("spark.some.config.option","some-value")
+        .enableHiveSupport()
+        .getOrCreate()
+    } else {
+      SparkSession
+        .builder()
+        .master("local[*]")
+        .appName(name)
+        .config("spark.some.config.option","some-value")
+        .getOrCreate()
+    }
+  }
 }
